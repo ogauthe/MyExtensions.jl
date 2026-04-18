a4b29aaf6873a9fad8c5387e8473cae28774e665
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-04-18 12:14:11 +0200

    typo

--- ext/MyExtensionsPEPSKitExt/MyExtensionsPEPSKitExt.jl
+++ ext/MyExtensionsPEPSKitExt/MyExtensionsPEPSKitExt.jl
@@ -3,7 +3,7 @@ module MyExtensionsPEPSKitExt
 using TensorKit: domain
 using PEPSKit: CTMRGEnv
 
-Base.show(io::IO, env::CTMRGEnv) = print(io, "CTMRGenv with unit cell ", size(env)[2:end])
+Base.show(io::IO, env::CTMRGEnv) = print(io, "CTMRGEnv with unit cell ", Base.tail(size(env)))
 
 function Base.show(io::IO, ::MIME"text/plain", env::CTMRGEnv)
     println(io, env)
